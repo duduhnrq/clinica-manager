ba2ab5568f24f54dfe9f6b17aeb30d3592a6c196
duduhnrq <dudu30rafa@gmail.com>
2025-10-24 10:47:20 -0300

    Add HelloController with /hello endpoint
    Introduces HelloController with a /hello GET endpoint that returns a success message. This provides a simple way to verify that the backend is running correctly.

---
+++ backend/src/main/java/br/com/clinica/backend/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.clinica.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Backend funcionando com sucesso!";
+    }
+}

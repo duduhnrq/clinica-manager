4544b121f193e083dc74da1ed45dca167b8f56e2
duduhnrq <dudu30rafa@gmail.com>
2025-10-25 12:06:45 -0300

    .

---
+++ frontend/clinica-frontend/.angular/cache/20.3.7/clinica-frontend/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-U6MXU7SO.js";
+import "./chunk-IJAOKHGK.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};

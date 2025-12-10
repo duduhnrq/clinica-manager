1a6288a24a5a7a5ded60810bf7895f884af1444b
duduhnrq <dudu30rafa@gmail.com>
2025-12-10 15:18:13 -0300

    Update Enviroments

--- frontend/clinica-frontend/src/app/services/paciente.service.ts
+++ frontend/clinica-frontend/src/app/services/paciente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
 
 export interface Endereco {
   rua: string;
@@ -31,7 +32,7 @@ export interface Paciente {
   providedIn: 'root',
 })
 export class PacienteService {
-  private apiUrl = 'http://localhost:8080/api/pacientes';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
--- frontend/clinica-frontend/src/environments/environment.prod.ts
+++ frontend/clinica-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://clinica-backend-66lv.onrender.com'
+  apiUrl: 'https://clinica-backend-66lv.onrender.com/api'
 };
